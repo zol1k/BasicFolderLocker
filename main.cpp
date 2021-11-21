eb70862747bbac1e01fcd2a118fc9cc7443bec11
zol1k <m.zoldos@gmail.com>
2021-11-22 00:57:16 +0100

    console msg

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello, world!";
+    string consoleTitle = "Console title";
+
+    SetConsoleTitleA(consoleTitle.c_str());
+    std::system("CLS");
+	std::cout << "Simple folder locker program just started...";
+    std::cout << "Enter a number you would like to count up to and press any key to start" << endl;
+
+    int choice = 0;
+    cin >> choice;
 	return 0;
 }
